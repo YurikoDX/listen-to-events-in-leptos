eff2959203a4cec397303365f4722d2dc6266e30
fuochai <fuochai@foxmail.com>
2025-10-19 22:55:35 +0800

    submenu

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,4 @@
-use tauri::{
-    // image::Image,
-    menu::{CheckMenuItemBuilder, IconMenuItemBuilder, MenuBuilder, SubmenuBuilder},
-};
+use tauri::menu::{MenuBuilder, SubmenuBuilder};
 
 use tauri::Manager;
 use tauri::Emitter;
@@ -9,9 +6,18 @@ use tauri::Emitter;
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
-            let menu = MenuBuilder::new(app)
+            let file_menu = SubmenuBuilder::new(app, "File")
                 .text("open", "Open")
-                .text("close", "Close")
+                .text("quit", "Quit")
+                .build()?;
+
+            let edit_menu = SubmenuBuilder::new(app, "Edit")
+                .text("copy", "Copy")
+                .text("paste", "Paste")
+                .build()?;
+
+            let menu = MenuBuilder::new(app)
+                .items(&[&file_menu, &edit_menu])
                 .build()?;
 
             let window = app.get_webview_window("main").unwrap();
@@ -20,21 +26,13 @@ pub fn run() {
             let app_handle = app.handle().clone();
 
             window.on_menu_event(move |_w, event| {
-                match event.id().as_ref() {
-                    "open"  => {
-                        println!(">>> Open clicked");
-                        app_handle.emit("menu-clicked", "open");
-                    },
-                    "close" => {
-                        println!(">>> Close clicked");
-                        app_handle.emit("menu-clicked", "close");
-                        // _w.close().unwrap();
-                    }
-                    _ => {}
-                }
+                let id = event.id().as_ref();
+                println!(">>> {} clicked", id);
+                app_handle.emit("menu-clicked", id).unwrap();
             });
 
             Ok(())
         })
-        .run(tauri::generate_context!());
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
